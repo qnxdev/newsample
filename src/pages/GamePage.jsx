2121221e9a0163d78f9fe80018c6006957f0eb4e
Dev <you@example.com>
2021-02-18 18:14:55 +0530

    Game

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { Router, useHistory } from "react-router-dom";
 import Page from "../components/Page";
@@ -5,10 +6,25 @@ import Page from "../components/Page";
 export default function AdmissionPage() {
   const [view, setView] = useState("initial");
   const [dime, setDime] = useState(2);
+  const [time, setTime] = useState(60);
 
+
+  useEffect(() => {
+    if(view=="game" && time > 0){
+        setTimeout(() => {
+            setTime(time - 1);
+          }, 1000);
+    }
+    if (time == 0) {
+      setView("error");
+    }
+  });
   let init = [2, 3, 4, 5];
   return (
     <Page>
+      <div className="timeout">
+        {view == "game" && <h1 id="time">{time}s <br/>Your score: {dime}</h1>}
+      </div>
       {view == "initial" && (
         <div className="initial">
           <h1>Start with</h1>
@@ -39,15 +55,14 @@ export default function AdmissionPage() {
       )}
       {view == "error" && (
         <div className="error">
-            <h1>Game Over !</h1>
+          <h1>Game Over !</h1>
         </div>
       )}
       <br />
       <br />
       <button
         onClick={() => {
-          setView("initial");
-          setDime(2);
+            window.location.reload();
         }}
       >
         Reset
@@ -57,8 +72,12 @@ export default function AdmissionPage() {
 }
 
 const GameTable = ({ dimension, pass, fail }) => {
-  let differentRow = Math.floor(Math.random() * dimension);
-  let differentCol = Math.floor(Math.random() * dimension);
+  const [differentRow, setRow] = useState(
+    Math.floor(Math.random() * dimension)
+  );
+  const [differentCol, setCol] = useState(
+    Math.floor(Math.random() * dimension)
+  );
 
   let rows = [];
   for (let i = 0; i < dimension; i++) {
@@ -71,8 +90,12 @@ const GameTable = ({ dimension, pass, fail }) => {
           }
           onClick={
             i == differentRow && j == differentCol
-              ? () => pass(dimension + 1)
-              : ()=>fail()
+              ? () => {
+                  pass(dimension + 1);
+                  setCol(Math.floor(Math.random() * dimension + 1));
+                  setRow(Math.floor(Math.random() * dimension + 1));
+                }
+              : () => fail()
           }
         >
           {j}
@@ -93,8 +116,8 @@ const GameTable = ({ dimension, pass, fail }) => {
           background: #222;
           color: red;
         }
-        td{
-            cursor: pointer;
+        td {
+          cursor: pointer;
         }
       `}</style>
     </>
